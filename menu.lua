4effdfe608e46750f68d000476ff9b428bee2ea7
Brice Thomas <brice.p.thomas@gmail.com>
2016-04-15 22:24:18 +0200

    Add menu music + smooth transition

--- menu.lua
+++ menu.lua
@@ -16,9 +16,15 @@ function Menu:initialize()
   self.map = sti.new("asset/menu.lua")
   self.world = bump.newWorld(1)
 
+  self.volume = { volume = 0.0 }
+
+  self.music = cache:getOrLoadSound("asset/music/menu.mp3")
+  self.music:setLooping(true)
+
   self.title = {
     displayed = false, opacity = 0,
-    pressEnterDisplayed = false, opacityPressEnter = 0
+    pressEnterDisplayed = false, opacityPressEnter = 0,
+    pressedEnter = false
   }
 
   self.menuTitle = cache:getOrLoadImage("asset/menu-title.png")
@@ -41,10 +47,15 @@ function Menu:initialize()
   self.baby = DummyBaby:new(self.world, baby.x, baby.y)
   self.baby.position = "swim"
 
+  self.moveBabyTween = tween.new(3, self.baby, {x = self.baby.x + 20})
+  self.showTitleTween = tween.new(2, self.title, { opacity = 255 } )
+  self.showPressEnterTween = tween.new(1, self.title, { opacityPressEnter = 255 } )
 
-  self.babyTween = tween.new(3, self.baby, {x = self.baby.x + 20})
-  self.titleTween = tween.new(1.5, self.title, { opacity = 255 } )
-  self.pressTween = tween.new(1, self.title, { opacityPressEnter = 255 } )
+  self.hideTitleTween = tween.new(1, self.title, { opacity = 0 } )
+  self.hidePressTween = tween.new(1, self.title, { opacityPressEnter = 0 } )
+
+  self.musicTween = tween.new(2, self.volume, { volume = 1.0 } )
+  self.lowerMusicTween = tween.new(2, self.volume, { volume = 0.0 } )
 
   local player = self.map.layers["player"].objects[1]
   self.player = DummyPlayer:new(self.world, player.x, player.y)
@@ -55,20 +66,36 @@ function Menu:update(dt)
   _.each(self.algaes, function (i, e) e:update(dt) end)
   self.baby:update(dt)
   self.player:update(dt)
-  local isBabyLost = self.babyTween:update(dt)
+  local isBabyLost = self.moveBabyTween:update(dt)
 
   if isBabyLost then
+    self.musicTween:update(dt)
+    self.music:setVolume(self.volume.volume)
+
+    self.music:play()
     self.title.displayed = true
-    local titleDisplayed = self.titleTween:update(dt)
+    local titleDisplayed = self.showTitleTween:update(dt)
 
     if titleDisplayed then
       self.title.pressEnterDisplayed = true
-      self.pressTween:update(dt)
+      self.showPressEnterTween:update(dt)
+    end
+  end
+
+  if self.title.pressedEnter then
+    local musicMute = self.lowerMusicTween:update(dt)
+    self.music:setVolume(self.volume.volume)
+
+    self.hidePressTween:update(dt)
+    self.hideTitleTween:update(dt)
+
+    if musicMute then
+      state:set(game)
     end
   end
 
   if love.keyboard.isDown("return") then
-    state:set(game)
+    self.title.pressedEnter = true
   end
 end
 
@@ -78,15 +105,11 @@ function Menu:draw()
   self.player:draw(self.title.displayed)
   self.baby:draw()
 
-  if self.title.displayed then
-    love.graphics.setColor(255, 255, 255, self.title.opacity)
-    love.graphics.draw(self.menuTitle)
-  end
+  love.graphics.setColor(255, 255, 255, self.title.opacity)
+  love.graphics.draw(self.menuTitle)
 
-  if self.title.pressEnterDisplayed then
-    love.graphics.setColor(255, 255, 255, self.title.opacityPressEnter)
-    love.graphics.draw(self.pressEnter)
-  end
+  love.graphics.setColor(255, 255, 255, self.title.opacityPressEnter)
+  love.graphics.draw(self.pressEnter)
 
   love.graphics.setColor(255, 255, 255, 255)
 end
